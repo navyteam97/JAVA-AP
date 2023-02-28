f137f97ce5cd442722f5c5fc98a2e77aeaa632c6
navyteam97 <axelivanmolina97@gmail.com>
2023-02-28 11:54:25 -0300

    primer commit

---
+++ src/miPrimerPaqueteJava/miPrimeraClase.java
@@ -0,0 +1,10 @@
+package miPrimerPaqueteJava;
+
+public class miPrimeraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wolrd");
+	}
+
+}
